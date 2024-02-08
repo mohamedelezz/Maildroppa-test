1f5a3e4db8434845c9cc2ede6ae79bc5cc307a16
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2024-02-09 01:25:18 +0200

    footer color

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white  w-full bottom-0 z-10">
+    <footer className="bg-white  w-full bottom-0 z-10 dark:bg-gray-800">
       <div className=" px-4 py-1 ">
         <div className="flex flex-col md:flex-row gap-2 md:gap-0  items-center justify-between ">
           <div className=" ">
@@ -20,12 +20,12 @@ export const Footer = () => {
             </Link>
           </div>
           <div className="">
-            <p className="text-gray-700">
+            <p className="text-gray-200">
               © 2020 Workflow. All rights reserved.
             </p>
           </div>
           <div className="">
-            <p className="text-gray-700">
+            <p className="text-gray-200">
               Made with ❤️ by Vercel
             </p>
           </div>
