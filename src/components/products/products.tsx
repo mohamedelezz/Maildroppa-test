3ae70db611d5015df2aea51f11c70595758d0ec4
Mohamed-AboElezz <mohamedelezz6@gmail.com>
2024-02-09 03:11:15 +0200

    dd

--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/typs";
 import { ProductCard } from ".";
 import Link from "next/link";
+import { shuffleArray } from "@/src/utils";
 
 export const Products = ({ products }: { products: Product[] }) => {
   return (
@@ -9,7 +10,7 @@ export const Products = ({ products }: { products: Product[] }) => {
         Our Products
       </h1>
       <div className="max-md:px-5 mb-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 ">
-        {products?.map((product) => (
+        {shuffleArray(products).map((product: Product) => (
           <Link href={`/product/${product.id}`} key={product.id}>
             <ProductCard product={product} key={product.id} />
           </Link>
